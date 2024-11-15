537a3216a3ae9452fedccc64c55e32dc6894f4db
jtalaveraa <jiramtalavera@hotmail.com>
2024-11-15 10:34:53 -0600

    Rename this field "$PDOlOCAL" to match the regular expression ^[a-z][a-zA-Z0-9]*$.

--- CLASS/database.php
+++ CLASS/database.php
@@ -1,15 +1,15 @@
 <?php
 class Database{
-    private $PDOlOCAL; // Si pones esta variable en null se cierra
+    private $pdolocal;
     private $user = 'root';
     private $password = '';
-    private $server = "mysql:host=localhost;dbname=la_sombra"; 
+    private $server = "mysql:host=localhost;dbname=la_sombra";
         
     function conectarBD()
     {
         try {
-            $this->PDOlOCAL = new PDO($this->server, $this->user, $this->password);
-            $this->PDOlOCAL->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Agregado para lanzar excepciones
+            $this->pdolocal = new PDO($this->server, $this->user, $this->password);
+            $this->pdolocal->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Agregado para lanzar excepciones
             } catch (PDOException $e) {
             echo $e->getMessage();
         }
@@ -18,7 +18,7 @@ class Database{
     function desconectarBD()
     {
         try {
-            $this->PDOlOCAL = null;
+            $this->pdolocal = null;
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
@@ -26,7 +26,7 @@ class Database{
 
     function getPDO()
     {
-        return $this->PDOlOCAL;
+        return $this->pdolocal;
     }
 
     function cerrarsesion()
@@ -36,4 +36,3 @@ class Database{
         header("Location: ../VIEWS/inicio-sesion.php");
     }
 }
-?>
